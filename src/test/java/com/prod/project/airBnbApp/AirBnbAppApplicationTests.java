2fead60439640755dcefaf4cc1ede2b1512585d1
Vivek Kumar <vivekkumar@Viveks-MacBook-Air.local>
2026-02-24 10:45:59 +0530

    Initial commit - Hotel Booking System Backend

---
+++ src/test/java/com/prod/project/airBnbApp/AirBnbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prod.project.airBnbApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirBnbAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
